325b8f7db9a80dd8303c4ffd5bae68b714751bc4
project2479 <jaywrsnp6@gmail.com>
2025-07-09 12:09:08 +0800

    new

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,4 +1,6 @@
-import { useState } from "react";import { MapContainer, TileLayer, useMapEvents, Marker, Popup } from "react-leaflet";import "leaflet/dist/leaflet.css";
+import { useState } from "react";
+import { MapContainer, TileLayer, useMapEvents, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 import api from "../assets/api";
 import Sidebar from "./Sidebar";
@@ -118,6 +120,18 @@ function Map() {
 												Name: {item.people} <br />
 												Age: {item.age} <br />
 												Gender: {item.gender}
+											</>
+										) : activeCategory === "households" ? (
+											<>
+												Family Name: {item.family_name} <br />
+												Members:
+												<ul className="list-disc ml-4">
+													{item.members.map((member, idx) => (
+														<li key={idx}>
+															{member.name} – {member.age} yrs – {member.role}
+														</li>
+													))}
+												</ul>
 											</>
 										) : (
 											"Unknown category"
