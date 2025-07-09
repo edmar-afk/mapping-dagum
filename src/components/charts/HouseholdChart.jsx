1333075603782dc23369680a5619f4b08f42047c
project2479 <jaywrsnp6@gmail.com>
2025-07-09 12:20:34 +0800

    new

--- src/components/charts/HouseholdChart.jsx
+++ src/components/charts/HouseholdChart.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";import api from "../../assets/api";
+
 function HouseholdChart() {
 	const [data, setData] = useState([]);
 
@@ -18,32 +19,48 @@ function HouseholdChart() {
 
 	return (
 		<>
-			<p className="text-center text-lg font-semibold my-12">
-				Household Members Around Brgy Matin-ao Bayog Zamboanga del Sur
-			</p>
+			<p className="text-center text-lg font-semibold my-12">Household Members Around Bayog Zamboanga del Sur</p>
 
 			<div className="space-y-10">
 				{data.map((household, index) => (
 					<div
 						key={index}
-						className="border p-4 rounded-lg shadow">
+						className="relative overflow-x-auto shadow-md sm:rounded-lg">
 						<h2 className="text-xl font-bold text-gray-800 mb-4 text-center">{household.family_name} Family</h2>
 
 						{household.members && household.members.length > 0 ? (
-							<table className="w-full table-auto border border-gray-300">
-								<thead className="bg-gray-100">
+							<table className="w-full text-sm text-left rtl:text-right text-gray-500">
+								<thead className="text-xs text-gray-700 uppercase">
 									<tr>
-										<th className="border px-4 py-2 text-left">Name</th>
-										<th className="border px-4 py-2 text-left">Age</th>
-										<th className="border px-4 py-2 text-left">Role</th>
+										<th
+											scope="col"
+											className="px-6 py-3 bg-gray-50">
+											Name
+										</th>
+										<th
+											scope="col"
+											className="px-6 py-3">
+											Age
+										</th>
+										<th
+											scope="col"
+											className="px-6 py-3 bg-gray-50">
+											Role
+										</th>
 									</tr>
 								</thead>
 								<tbody>
 									{household.members.map((member, i) => (
-										<tr key={i}>
-											<td className="border px-4 py-2">{member.name}</td>
-											<td className="border px-4 py-2">{member.age}</td>
-											<td className="border px-4 py-2">{member.role}</td>
+										<tr
+											key={i}
+											className="border-b border-gray-200">
+											<th
+												scope="row"
+												className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50">
+												{member.name}
+											</th>
+											<td className="px-6 py-4">{member.age}</td>
+											<td className="px-6 py-4 bg-gray-50">{member.role}</td>
 										</tr>
 									))}
 								</tbody>
@@ -56,7 +73,6 @@ function HouseholdChart() {
 			</div>
 		</>
 	);
-	
 }
 
 export default HouseholdChart;
